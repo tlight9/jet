918a1372c80ea0406e7c367a8298eaee43972e98
John Thornton <dev@gnipsel.com>
2024-02-13 07:24:31 -0600

    work on hal
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- jet/src/libjet/startup.py
+++ jet/src/libjet/startup.py
@@ -153,11 +153,6 @@ def postgui_hal(parent):
 			if res: raise SystemExit(res)
 
 def setup_hal(parent):
-	parent.hal_test = hal.component('test')
-	parent.hal_test.newpin('out', hal.HAL_BIT, hal.HAL_OUT)
-	parent.hal_test.ready()
-
-	'''
 	for button in parent.findChildren(QPushButton):
 		if button.property('function') == 'hal_pin':
 			#print(f'{button.objectName()} {button.property("function")}')
@@ -165,17 +160,57 @@ def setup_hal(parent):
 			for prop in props:
 				prop = str(prop, 'utf-8')
 				if prop.startswith('pin_'):
+					#print(f'prop {prop}')
 					pin_settings = button.property(prop).split(',')
 					name = button.objectName()
-					setattr(parent, f'hal_{name}', None)
-					setattr(parent, f'hal_{name}', hal.component(f'{name}'))
+					#print(f'name {name}')
 					pin_name = pin_settings[0]
+					#print(f'pin_name {pin_name}')
 					pin_type = getattr(hal, f'{pin_settings[1].upper().strip()}')
+					#print(f'pin_type {pin_type}')
 					pin_dir = getattr(hal, f'{pin_settings[2].upper().strip()}')
-					getattr(parent, f'hal_{name}').newpin(pin_name, pin_type, pin_dir)
+					#print(f'pin_dir {pin_dir}')
+					parent.hal_comp = hal.component('jet')
+					#print(f'{parent.hal_comp.getprefix()}')
+					setattr(parent, f'{prop}', parent.hal_comp.newpin(pin_name, pin_type, pin_dir))
+					#print(getattr(parent, f'{prop}').name)
+					print(parent.hal_comp[pin_name])
+					print(parent.hal_comp.getpins())
+					print(dir(parent.hal_comp))
+					#print(f'parent.{prop}')
+					#pin = parent.hal_comp.newpin(pin_name, pin_type, pin_dir)
+					#print(f'{parent.hal_comp.getpins()}')
+					#self.test_hal.toggled.connect(lambda: self.out2.set(self.test_hal.isChecked()))
+					#button.clicked.connect(lambda: getattr(parent, f'{prop}').name.set(True))
+					#button.pressed.connect(lambda: hal.set_p(f'parent.{prop}', "True"))
+					#button.toggled.connect(lambda: parent.hal_comp[pin_name], button.isChecked())
+
+
+					#hal.component(f'{name}')
+					# self.out2 = self.halcomp.newpin('out2', hal.HAL_BIT, hal.HAL_OUT)
+					#test
+					#print(f'test {test}')
+
+
+					#print(f'name {name}')
+					#setattr(parent, f'hal_{name}', hal.component(f'{name}'))
+
+					'''
+					setattr(parent, f'hal_{name}', None)
+					test = 
+					print(f'pin_name {pin_name}')
 					getattr(parent, f'hal_{name}').ready()
-					print(button)
-					button.clicked.connect(getattr(parent, f'hal_{name}'))
+					#print(button) self.out2.set(self.test_hal.isChecked()))
+					
+					'''
+
+
+	'''
+
+	parent.hal_test = hal.component('test')
+	parent.hal_test.newpin('out', hal.HAL_BIT, hal.HAL_OUT)
+	parent.hal_test.ready()
+	parent.test_hal.pressed.connect(lambda: hal.set_p('test.out', "true"))
 
 
 	#setattr(parent, f'c_{i}', None)
